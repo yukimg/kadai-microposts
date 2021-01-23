c20a4986e9155018f0e5226cf94a4ce6a5365e13
yuki <yuu.maru.571718@gmail.com>
2021-01-23 08:38:50 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-    root to: 'toppages#index'
+  root to: 'toppages#index'
+    
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
