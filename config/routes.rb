13e8d10523338f07e99539b86a303208ede7f363
yuki <yuu.maru.571718@gmail.com>
2021-01-24 09:16:27 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,14 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+    
+  end
   
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
